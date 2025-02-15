07da18973a488983b426c2ff7dfd459b78ac625d
Birhan <birhan.coder7@gmail.com>
2025-02-15 13:48:26 +0300

    banner

--- netflix-clone/src/utils/requests.jsx
+++ netflix-clone/src/utils/requests.jsx
@@ -1,6 +1,6 @@
-const API_KEY = process.env.REACT_APP_API_KEY; 
+const API_KEY = import.meta.env.VITE_API_KEY;
 const requests = {
-  fetchTrending: `/trending/all/qweek?api_key=${API_KEY}&language=en-US`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
@@ -8,7 +8,9 @@ const requests = {
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-  fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+  fetchTvShow: `/tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
 };
 
 export default requests;
+
+// poster: https://image.tmdb.org/t/p/original/3Z9c1tbUhP0QruRjczPHnbx3U2D.jpg
