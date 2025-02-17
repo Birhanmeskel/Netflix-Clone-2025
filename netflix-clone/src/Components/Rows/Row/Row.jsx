f25abd83b206f7c281a668e26cf5d136faa6f91a
Birhan <birhan.coder7@gmail.com>
2025-02-17 07:16:46 +0300

    movies trend added

--- netflix-clone/src/Components/Rows/Row/Row.jsx
+++ netflix-clone/src/Components/Rows/Row/Row.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./row.css";
 import axios from "../../../utils/axios";
+import movieTrailer from "movie-trailer";
+import YouTube from "react-youtube";
 
 const Row = ({ title, fetchUrl, isLargeRow }) => {
-  const [movie, setMovie] = useState([]);
-  //   const [trailerUrl, setTrailerUrl] = useState("");
+  const [movies, setMovie] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   const base_url = "https://image.tmdb.org/t/p/original";
 
@@ -19,22 +21,53 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
       }
     };
     fetchData();
-  }, []);
+  }, [fetchUrl]);
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || movie?.original_name || movie?.title).then(
+        (url) => {
+          console.log(url);
+          const urlparams = new URLSearchParams(new URL(url).search);
+          console.log(urlparams);
+          setTrailerUrl(urlparams.get("v"));
+          console.log(urlparams.get("v"));
+        }
+      );
+    }
+  };
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
   return (
     <div className="row">
       <h1>{title}</h1>
       <div className="row_posters">
-        {movie?.map((movie, index) => (
+        {movies?.map((movie, index) => (
           <img
             key={index}
-            // onClick={()=>setTrailerUrl(movie.id)}
+            onClick={() => handleClick(movie)}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
-            alt={movie.name}
+            alt={
+              movie?.name ||
+              movie?.original_name ||
+              movie?.title ||
+              "Movie Poster"
+            }
             className={`row_poster ${isLargeRow && "row_posterLarge"}`}
           />
         ))}
+      </div>
+      <div style={{ padding: "40px" }}>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
       </div>
     </div>
   );
