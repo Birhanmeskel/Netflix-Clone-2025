3a0a9c76649195ad8c8e9d9a71f2b460ea8b8d3b
Birhanmeskel <birhan.coder7@gmail.com>
2025-07-25 13:01:02 +0300

    deploy on netlify

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -1,8 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Netflix-Clone-2025/', // <--- Add this line
+  // Remove base path for Netlify deployment
 });
